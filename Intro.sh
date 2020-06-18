7864003c80838b3aab81e1209758aa125e62dbba
Cyber bocah 08 <66514438+CyberIndoBocah08@users.noreply.github.com>
2020-06-18 13:27:37 +0700

    Add files via upload

---
+++ Intro.sh
@@ -0,0 +1,17 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+print'kenapa kamu bukak goblok'
+echo'======================================================='
+echo'this my intro'
+echo'MyName:MrBocah08'
+echo'MyTeam: CyberIndoBocah08'
+echo'MyKontak:085768567403'
+echo:myGrupWA:https://chat.whatsapp.com/Lv636h3pWzABFuUK0fChUq
+echo'======================================================='
+print'Makasih atas supportnya'
